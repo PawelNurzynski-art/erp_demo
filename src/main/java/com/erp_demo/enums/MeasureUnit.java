c5ba5f8804b849ef855bd82e52fe7f21ccce797a
PawelNurzynski-art <you@example.com>
2023-03-05 16:05:09 +0100

    Changes: 1. error: java.lang.IllegalArgumentException: No enum constant com.erp_demo.enums.MeasureUnit.szt. - was caused by szt. in measure_unit column (data in 006_data_to_product_table.csv), changed to SZT fixed the problem

--- src/main/java/com/erp_demo/enums/MeasureUnit.java
+++ src/main/java/com/erp_demo/enums/MeasureUnit.java
@@ -2,15 +2,4 @@ package com.erp_demo.enums;
 
 public enum MeasureUnit {
     SZT;
-//    SZT("szt.");
-
-//    private final String measureUnit;
-//
-//    MeasureUnit(String measureUnit) {
-//        this.measureUnit = measureUnit;
-//    }
-//
-//    public String getMeasureUnit() {
-//        return measureUnit;
-//    }
 }
